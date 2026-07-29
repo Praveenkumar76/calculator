7e21e106b4bd295628f2f2de703a4a30c1f27a69
admin <admin@admins-MacBook-Air.local>
2026-07-29 13:30:14 +0530

    Configure SonarQube to import JaCoCo coverage

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -7,6 +7,7 @@ mvn clean verify sonar:sonar \
   -Dsonar.projectKey=calculator \
   -Dsonar.host.url=http://host.docker.internal:9000 \
   -Dsonar.login=squ_d97765630e79de0839c01c47ed64ed933fa45d33 \
+  -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml \
   -Dsonar.qualitygate.wait=true
 
 echo "========== SONAR SUCCESS =========="
