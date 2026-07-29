0a1c783dd4e19824cf26068f4335b5660ee4ab72
admin <admin@admins-MacBook-Air.local>
2026-07-29 12:51:44 +0530

    Add SonarQube stage to CI/CD pipeline

---
+++ scripts/sonar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "========== SONAR START =========="
+
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=calculator \
+  -Dsonar.host.url=http://host.docker.internal:9000 \
+  -Dsonar.login=squ_d97765630e79de0839c01c47ed64ed933fa45d33 \
+  -Dsonar.qualitygate.wait=true
+
+echo "========== SONAR SUCCESS =========="
